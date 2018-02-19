2e565597f338618c3266b49ee1f771268bb83134
Ja1r0 <yangjairo@gmail.com>
2018-02-19 09:15:46 +0800

    Update SumTree.py

--- SumTree.py
+++ SumTree.py
@@ -93,6 +93,17 @@ class Solution:
 #########
 # 3 Sum #
 #########
+'''
+Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? 
+Find all unique triplets in the array which gives the sum of zero.
+Note: The solution set must not contain duplicate triplets.
+For example, given array S = [-1, 0, 1, 2, -1, -4],
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+'''
 class Solution:
     def threeSum(self, nums):
         """
@@ -101,22 +112,24 @@ class Solution:
         """
         solution=[]
         nums.sort()
-        for i in range(len(nums)-2):       
+        for i in range(len(nums)-2):
             if i>0 and nums[i]==nums[i-1]:
-                continue
-            target=0-nums[i]
-            numbers=nums[i+1:]
-            s=set()
-            for value in numbers:
-                if target-value in s:
-                    solution.append([nums[i],value,target-value])
+                continue            
+            l=i+1
+            r=len(nums)-1
+            while l<r:
+                sum=nums[i]+nums[l]+nums[r]
+                if sum<0:
+                    l+=1
+                elif sum>0:
+                    r-=1
                 else:
-                    s.add(value)
-        final_solution=[]
-        for s in solution:
-            if set(s) not in final_solution:
-                final_solution.append(set(s))
-        sol=[]
-        for e in final_solution:
-            sol.append(list(e).sort())
-        return sol
+                    solution.append([nums[i],nums[l],nums[r]])
+                    while l<r and nums[l]==nums[l+1]:
+                        l+=1
+                    while l<r and nums[r]==nums[r-1]:
+                        r-=1
+                    l+=1
+                    r-=1                    
+        return solution
+                
