80d5d81b94eaf14647e93ebd4f5f15a75f7e297a
Ja1r0 <yangjairo@gmail.com>
2018-02-20 22:41:22 +0800

    Update SumTree.py

--- SumTree.py
+++ SumTree.py
@@ -132,4 +132,46 @@ class Solution:
                     l+=1
                     r-=1                    
         return solution
-                
+########
+# 4sum #
+########
+'''
+Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target?
+Find all unique quadruplets in the array which gives the sum of target.
+Note: The solution set must not contain duplicate quadruplets.
+For example, given array S = [1, 0, -1, 0, -2, 2], and target = 0.
+A solution set is:
+[
+  [-1,  0, 0, 1],
+  [-2, -1, 1, 2],
+  [-2,  0, 0, 2]
+]
+'''
+class Solution:
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        l=len(nums)
+        res=set()
+        d={}
+        if l<4:
+            return []
+        for i in range(l-1):
+            for j in range(i+1,l):
+                sum2=nums[i]+nums[j]
+                if sum2 in d:
+                    d[sum2].append((i,j))
+                else:
+                    d[sum2]=[(i,j)]
+        for i in range(l-3):
+            for j in range(i+1,l-2):
+                sum2=target-nums[i]-nums[j]
+                if sum2 in d:
+                    for t in d[sum2]:
+                        if t[0]>j:
+                            res.add((nums[i],nums[j],nums[t[0]],nums[t[1]]))
+        return [list(t) for t in res]
