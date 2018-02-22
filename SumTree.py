45b81a9968516ca3ed8d844ffe2bd92abdd51f1c
Ja1r0 <yangjairo@gmail.com>
2018-02-22 10:04:40 +0800

    Update SumTree.py

--- SumTree.py
+++ SumTree.py
@@ -175,3 +175,52 @@ class Solution:
                         if t[0]>j:
                             res.add((nums[i],nums[j],nums[t[0]],nums[t[1]]))
         return [list(t) for t in res]
+###########
+# 4sum II #
+###########
+'''
+Given four lists A, B, C, D of integer values, 
+compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. 
+All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+Example:
+Input:
+A = [ 1, 2]
+B = [-2,-1]
+C = [-1, 2]
+D = [ 0, 2]
+Output:
+2
+Explanation:
+The two tuples are:
+1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+'''
+class Solution:
+    def fourSumCount(self, A, B, C, D):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :type C: List[int]
+        :type D: List[int]
+        :rtype: int
+        """
+        A.sort()
+        B.sort()
+        C.sort()
+        D.sort()
+        d={}
+        res=0
+        l=len(A)
+        for i in range(l):
+            for j in range(l):
+                if C[i]+D[j] not in d:
+                    d[C[i]+D[j]]=[(i,j)]
+                else:
+                    d[C[i]+D[j]].append((i,j))
+        for i in range(l):
+            for j in range(l):
+                sum2=0-A[i]-B[j]
+                if sum2 in d:
+                    res+=len(d[sum2])
+        return res
